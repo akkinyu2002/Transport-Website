23affc2cbf3629d6585c6412bfe5e8015992aa9c
Aakash Neupane <neupaneakash687@gmail.com>
2026-09-17 20:13:42 +0545

    feat: centralize business information

---
+++ src/data/business.js
@@ -0,0 +1,12 @@
+export const business = {
+  name: 'President Limo Australia',
+  type: 'Limousine Service',
+  city: 'Melbourne, Australia',
+  address: '2/430 Little Collins St, Melbourne VIC 3000, Australia',
+  phone: '+61 1800 986 658',
+  phoneHref: 'tel:+611800986658',
+  availability: 'Open 24 hours',
+  rating: '4.6',
+  reviewCount: 10,
+  mapsHref: 'https://www.google.com/maps/search/?api=1&query=2%2F430%20Little%20Collins%20St%2C%20Melbourne%20VIC%203000',
+}
